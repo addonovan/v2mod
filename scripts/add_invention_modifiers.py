5e7b6f77a919713fa2b5ccd0396a476ca4305e4a
addonovan <addonovan@users.noreply.github.com>
2017-05-03 15:33:00 -0500

    Updated script to ask if military or not

--- scripts/add_invention_modifiers.py
+++ scripts/add_invention_modifiers.py
@@ -4,7 +4,8 @@ from os.path import isfile
 r_invention = re.compile( '^([a-zA-z]*?) = {' )
 r_rename = re.compile( '\$NAME\$' )
 
-modifier = "\tmodifier = { factor = 5  any_neighbor_country = { invention = $NAME$ } }\n"
+nonmilitary_modifier = "\tmodifier = { factor = 5  any_neighbor_country = { invention = $NAME$ } }\n"
+military_modifier = "\tmodifier = { factor = 1 war = yes }\n"
 
 def is_block_start( text ):
 	result = r_invention.search( text )
@@ -37,7 +38,7 @@ def match_brace( text ):
 
 	return block
 
-def add_modifier( name, block ):
+def add_modifier( name, block, modifier ):
 	# find just the chance block
 	chance_index = block.find( "chance = {" )
 	pre_chance = block[ 0 : chance_index ]
@@ -49,7 +50,7 @@ def add_modifier( name, block ):
 
 	return pre_chance + r_rename.sub( name, chance ) + post_chance
 
-def search_file( file ):
+def search_file( file, modifier ):
 	with open( file ) as f:
 		content = f.read();
 
@@ -63,7 +64,7 @@ def search_file( file ):
 		if result:
 			name = result.group( 1 )
 			block = match_brace( content )
-			output += add_modifier( name, block )
+			output += add_modifier( name, block, modifier )
 			
 			content = content[ len( block ) + 1 : ]
 
@@ -84,6 +85,14 @@ while True:
 	file = folder + name
 
 	if isfile( file ):
+
+		# default to the nonmilitary invention modifier
+		modifier = nonmilitary_modifier
+
+		# if the user says that these are military techs, then change the modifier
+		if input( "Are these military techs? (y/n)  " )[ 0 ] == 'y':
+			modifier = military_modifier
+
 		print( "Parsing {}".format( file ) )
 		content = search_file( file )
 
@@ -94,3 +103,6 @@ while True:
 	else:
 		print( "That's not an existing file" )
 		break
+
+	# Print new lines for prettification
+	print( "\n\n\n" )
